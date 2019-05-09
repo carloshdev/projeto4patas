98baad0827bde9ec78c45e06de31ee308aa5fdcb
LuizaoFavela7 <luizfms2008@gmail.com>
2019-05-09 15:18:47 -0300

    Tela de Login e Cadastro

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/luiz', function () {
+    return view('luiz');
+});
+
+Auth::routes();
+
+Route::get('/auth/login', 'HomeController@index')->name('home');
