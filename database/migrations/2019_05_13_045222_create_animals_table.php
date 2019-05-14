e0d142fc4ebad4da2af86dd9dfea4405d8995bf2
Carlos Henrique <carloshm.ribeiro@gmail.com>
2019-05-13 21:37:20 -0300

    Criação migrates database

---
+++ database/migrations/2019_05_13_045222_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->bigIncrements('id_animal');
+            $table->string('name');
+            $table->string('breed');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
